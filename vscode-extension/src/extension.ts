9de1be4b282ed77eea9f8ba5d2e6c12a63105974
Florian Gareis <flo.gareis@tum.de>
2018-12-05 13:55:11 +0100

    Rewrite tooling, vscode and theia code for new lint

--- vscode-extension/src/extension.ts
+++ vscode-extension/src/extension.ts
@@ -1,3 +1,7 @@
+// tslint:disable:no-var-requires
+// tslint:disable:no-require-imports
+// tslint:disable:no-use-before-declare
+
 'use strict';
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
@@ -6,12 +10,14 @@ const tooling = require('jsonforms-tooling');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export const activate = (context: vscode.ExtensionContext) => {
 
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with  registerCommand
   // The commandId parameter must match the command field in package.json
-  const createExampleProject = vscode.commands.registerCommand('extension.createExampleProject', (args: any) => {
+  const createExampleProject = vscode.commands.registerCommand(
+    'extension.createExampleProject',
+    (args: any) => {
     if (!args) {
       const options: vscode.OpenDialogOptions = {
         canSelectMany: false,
@@ -19,8 +25,8 @@ export function activate(context: vscode.ExtensionContext) {
         canSelectFiles: false,
         openLabel: 'Select folder',
       };
-      vscode.window.showOpenDialog(options).then((fileUri) => {
-        if (fileUri && fileUri[0]) {
+      vscode.window.showOpenDialog(options).then(fileUri => {
+        if (fileUri && fileUri[0].fsPath) {
           asyncCreateExampleProject(fileUri[0].fsPath);
         } else {
           showMessage('Please select a empty folder', 'err');
@@ -32,7 +38,9 @@ export function activate(context: vscode.ExtensionContext) {
     }
   });
 
-  const createSeedProject = vscode.commands.registerCommand('extension.createSeedProject', (args: any) => {
+  const createSeedProject = vscode.commands.registerCommand(
+    'extension.createSeedProject',
+    (args: any) => {
     if (!args) {
       const options: vscode.OpenDialogOptions = {
         canSelectMany: false,
@@ -40,8 +48,8 @@ export function activate(context: vscode.ExtensionContext) {
         canSelectFiles: false,
         openLabel: 'Select folder',
       };
-      vscode.window.showOpenDialog(options).then((fileUri) => {
-        if (fileUri && fileUri[0]) {
+      vscode.window.showOpenDialog(options).then(fileUri => {
+        if (fileUri && fileUri[0].fsPath) {
           asyncCreateSeedProject(fileUri[0].fsPath);
         } else {
           showMessage('Please select a empty folder', 'err');
@@ -53,7 +61,9 @@ export function activate(context: vscode.ExtensionContext) {
     }
   });
 
-  const generateUISchema = vscode.commands.registerCommand('extension.generateUISchema', (args: any) => {
+  const generateUISchema = vscode.commands.registerCommand(
+    'extension.generateUISchema',
+    (args: any) => {
     if (!args) {
       const options: vscode.OpenDialogOptions = {
         canSelectMany: false,
@@ -64,8 +74,8 @@ export function activate(context: vscode.ExtensionContext) {
           'Json Files': ['json'],
         },
       };
-      vscode.window.showOpenDialog(options).then((fileUri) => {
-        if (fileUri && fileUri[0]) {
+      vscode.window.showOpenDialog(options).then(fileUri => {
+        if (fileUri && fileUri[0].fsPath) {
           asyncGenerateUiSchema(fileUri[0].fsPath);
         } else {
           showMessage('Please select a json schema file', 'err');
@@ -80,48 +90,53 @@ export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(createExampleProject);
   context.subscriptions.push(createSeedProject);
   context.subscriptions.push(generateUISchema);
-}
+};
 
 /**
  * Async Creating Example Project
  * @param {string} path the path to the project folder
  */
-function asyncCreateExampleProject(path: string) {
+const asyncCreateExampleProject = (path: string) => {
   showMessage(`Creating example project: ${path}`);
   tooling.cloneAndInstall('example', path, (result: string, type: string) => {
     showMessage(result, type);
   });
-}
+};
 
 /**
  * Async Creating Seed Project
  * @param {string} path the path to the project folder
  */
-function asyncCreateSeedProject(path: string) {
+const asyncCreateSeedProject = (path: string) => {
   const options: vscode.InputBoxOptions = {
     prompt: 'Label: ',
     placeHolder: 'Enter a name for your seed project',
   };
-  vscode.window.showInputBox(options).then((name) => {
+  vscode.window.showInputBox(options).then(name => {
     let projectName = name;
     if (!name) {
       projectName = 'jsonforms-seed';
     }
     showMessage(`Creating seed project: ${path}`);
-    tooling.cloneAndInstall('seed', path, (result: string, type: string) => { showMessage(result, type); }, projectName);
+    tooling.cloneAndInstall(
+      'seed',
+      path,
+      (result: string, type: string) => { showMessage(result, type); },
+      projectName
+    );
   });
-}
+};
 
 /**
  * Async Generate UI Schema
  * @param {string} path the path to the project folder
  */
-function asyncGenerateUiSchema(path: string) {
+const asyncGenerateUiSchema = (path: string) => {
   const options: vscode.InputBoxOptions = {
     prompt: 'Label: ',
     placeHolder: 'Enter a filename for your UI Schema (default: ui-schema.json)',
   };
-  vscode.window.showInputBox(options).then((name) => {
+  vscode.window.showInputBox(options).then(name => {
     let fileName = name;
     if (!name) {
       fileName = 'ui-schema.json';
@@ -131,14 +146,14 @@ function asyncGenerateUiSchema(path: string) {
       showMessage(result, type);
     });
   });
-}
+};
 
 /**
  * Show Visual Studio Code Message
  * @param {string} message the message that should be displayed
  * @param {string} type the type of the message
  */
-function showMessage(message: string, type?: string) {
+const showMessage = (message: string, type?: string) => {
   switch (type) {
     case 'err':
       vscode.window.showErrorMessage(message);
@@ -149,8 +164,4 @@ function showMessage(message: string, type?: string) {
     default:
       vscode.window.showInformationMessage(message);
   }
-}
-
-// this method is called when your extension is deactivated
-export function deactivate() {
-}
+};
