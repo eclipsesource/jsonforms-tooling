816e94906211eb066ebaab01d3bf7863cee68c46
Florian Gareis <mail@zoker.me>
2019-03-18 13:52:11 +0100

    Split common code into components

---
+++ jsonforms-tooling-common/src/utils.ts
@@ -0,0 +1,41 @@
+import { readdir, readFile, writeFile } from 'fs';
+import Ajv from 'ajv';
+import { promisify } from 'util';
+import { uiMetaSchema } from './metaSchema';
+
+export const readFileWithPromise = promisify(readFile);
+export const readdirWithPromise = promisify(readdir);
+export const writeFileWithPromise = promisify(writeFile);
+
+/**
+ * Validate a given JSON Schema
+ * @param {Object} schema the json schema, that will be validated
+ * @param {function} callback forwards the current status to the caller
+ */
+export const validateUiSchema = async (schema: Object) => {
+  try {
+    const ajv =  new Ajv();
+    return ajv.validate(uiMetaSchema, schema);
+  } catch (error) {
+    throw(error.message);
+  }
+};
+
+/**
+ * Show message within the editor
+ * @param {any} editorInstance the instance of the editor
+ * @param {string} message the message that should be displayed
+ * @param {string} type the optional type of the message
+ */
+export const showMessage = (editorInstance: any, message: string, type?: string) => {
+  switch (type) {
+    case 'err':
+      editorInstance.window.showErrorMessage(message);
+      break;
+    case 'war':
+      editorInstance.window.showWarningMessage(message);
+      break;
+    default:
+      editorInstance.window.showInformationMessage(message);
+  }
+};
